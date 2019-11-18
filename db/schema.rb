55c7093e256b213e959cc988ac16a33b783a94a1
venelrene <venelrene@gmail.com>
2019-11-17 22:27:02 -0500

    change colume name from body to content in the post table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191026025538) do
+ActiveRecord::Schema.define(version: 20191118032356) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.text "body"
+    t.text "content"
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
